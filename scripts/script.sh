9c6dacd144a16179f4a047531814eccf26980365
Mohamed <mohamed.anwar@cern.ch>
2026-02-05 18:46:36 +0100

    few fixes

---
+++ scripts/script.sh
@@ -0,0 +1,2 @@
+time python3 makeHistogramsFor2DAlphabetMthodPost_VBF.py 20250814_DataMC 2017 VBFjj               
+time python3 makeHistogramsFor2DAlphabetMthodPost_VBF.py 20250814_DataMC 2018 VBFjj               
